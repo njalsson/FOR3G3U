ef1406be728bbb6b5ffad07ca1ef0b92cc3b7d84
njalsson <fridriknj339@nemi.tskoli.is>
2017-05-01 21:21:06 +0000

    1337

--- verk2/main.py
+++ verk2/main.py
@@ -1,2 +1,22 @@
 import pygame
 from pygame.locals import *
+
+"""
+leikur 1 i verk 2
+game of dice
+"""
+pygame.init()
+gamescreen = pygame.display.set_mode((640,480))
+pygame.display.set_caption('dices')
+
+running = True
+"""
+main game loop
+"""
+
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+pygame.quit()
+quit()
