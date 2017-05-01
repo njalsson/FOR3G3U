359d363dfdbf614629f1eb55d88fc54055cc4302
njalsson <fridriknj339@nemi.tskoli.is>
2017-05-01 21:27:53 +0000

    added images

--- verk2/main.py
+++ verk2/main.py
@@ -8,7 +8,12 @@ game of dice
 pygame.init()
 gamescreen = pygame.display.set_mode((640,480))
 pygame.display.set_caption('dices')
-
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+purple = (100,0,230)
 running = True
 """
 main game loop
@@ -18,5 +23,9 @@ while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
+			
+	gamescreen.fill(purple)
+	pygame.display.update()
+
 pygame.quit()
 quit()
